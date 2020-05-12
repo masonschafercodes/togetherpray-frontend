a69105de550b665cb27c0ae53d5c606d506e42fa
Mason Schafer <mason.schafer1@gmail.com>
2020-05-11 21:44:07 -0400

    conditional jumbotron on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,7 +29,7 @@ class Home extends Component{
                         <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
                         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
                         <p className="lead">
-                        <Button className="jumbotron-button" href="/login">Interact!</Button>
+                        <Button className="jumbotron-button" href="/read">Read the Verse of the Day</Button>
                         </p>
                     </Jumbotron>
                 </div>
