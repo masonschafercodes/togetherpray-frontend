36b70f50d8caa244b1452c2daa4a5c65f0618a29
Mason Schafer <mason.schafer1@gmail.com>
2020-05-11 21:42:04 -0400

    conditional jumbotron on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import NavigationBar from './Navbar';
 import { Jumbotron, Button } from 'reactstrap';
+import firebase from 'firebase';
 
 
 class Home extends Component{
+    state={isSignedIn: false};
+
+    componentDidMount = () => {
+      firebase.auth().onAuthStateChanged(user => {
+          this.setState({isSignedIn:!!user})
+      })
+    }
     render(){
         return(
 
@@ -14,14 +22,27 @@ class Home extends Component{
                     </nav> 
                 </div>
                 <div className="hero-background-color">
-                <Jumbotron>
-                    <h1 className="display-3 caption-font">Welcome to Verse With Us</h1>
-                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <p className="lead">
-                    <Button className="jumbotron-button" href="/login">Get Started!</Button>
-                    </p>
-                </Jumbotron>
+                    {this.state.isSignedIn ?
+                <div>
+                    <Jumbotron>
+                    <h1 className="display-3 caption-font">Welcome, {firebase.auth().currentUser.displayName}</h1>
+                        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                        <p className="lead">
+                        <Button className="jumbotron-button" href="/login">Interact!</Button>
+                        </p>
+                    </Jumbotron>
+                </div>
+                    :
+                    <Jumbotron>
+                        <h1 className="display-3 caption-font">Welcome to Verse With Us</h1>
+                        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                        <p className="lead">
+                        <Button className="jumbotron-button" href="/login">Get Started!</Button>
+                        </p>
+                    </Jumbotron>
+            }
                 </div>
             </div>
         )
